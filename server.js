e2134190118ba6570f60e5925a7d11f45c1c9355
Sekouba CAMARA <sekoubacamara100@gmail.com>
2026-03-21 17:37:13 -0400

    Connected the server

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
+import express from 'express'
+import './db/connection.js'
+import productRoutes from './routes/productRoutes.js'
 
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+
+app.use('/api/products', productRoutes)
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.listen(PORT, () => {
+    console.log('Server is listening on port: ', PORT)
+})
 
 
 
